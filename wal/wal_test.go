7236baf5144c695c9ea22d0f3204650bbc4127ec
Tej Chajed <tchajed@mit.edu>
2020-02-05 10:04:54 -0500

    Remove note on TestRecoverFlushed

--- wal/wal_test.go
+++ wal/wal_test.go
@@ -189,8 +189,6 @@ func (suite *WalSuite) TestShutdownInProgress() {
 	l.Shutdown()
 }
 
-// Disabled for now because it uses low block numbers that interfere with the
-// log's on-disk storage.
 func (suite *WalSuite) TestRecoverFlushed() {
 	l := suite.l
 	l.MemAppend(contiguousTxn(1, 3, block1))
