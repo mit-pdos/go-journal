431aa09dcda67fa7c03ad59cea2bb385f8102537
Tej Chajed <tchajed@mit.edu>
2021-05-08 11:22:29 -0400

    Use a map to track acquired addresses

--- twophase/twophase.go
+++ twophase/twophase.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/mit-pdos/goose-nfsd/addr"
 	"github.com/mit-pdos/goose-nfsd/buftxn"
-	"github.com/mit-pdos/goose-nfsd/common"
 	"github.com/mit-pdos/goose-nfsd/lockmap"
 	"github.com/mit-pdos/goose-nfsd/txn"
 	"github.com/mit-pdos/goose-nfsd/util"
@@ -19,7 +18,7 @@ type TwoPhasePre struct {
 type TwoPhase struct {
 	buftxn   *buftxn.BufTxn
 	locks    *lockmap.LockMap
-	acquired []uint64
+	acquired map[uint64]bool
 }
 
 func Init(d disk.Disk) *TwoPhasePre {
@@ -35,7 +34,7 @@ func Begin(twophasePre *TwoPhasePre) *TwoPhase {
 	trans := &TwoPhase{
 		buftxn:   buftxn.Begin(twophasePre.txn),
 		locks:    twophasePre.locks,
-		acquired: make([]common.Bnum, 0),
+		acquired: make(map[uint64]bool),
 	}
 	util.DPrintf(5, "tp Begin: %v\n", trans)
 	return trans
@@ -44,18 +43,12 @@ func Begin(twophasePre *TwoPhasePre) *TwoPhase {
 func (twophase *TwoPhase) acquireNoCheck(addr addr.Addr) {
 	flatAddr := addr.Flatid()
 	twophase.locks.Acquire(flatAddr)
-	twophase.acquired = append(twophase.acquired, flatAddr)
+	twophase.acquired[flatAddr] = true
 }
 
 func (twophase *TwoPhase) isAlreadyAcquired(addr addr.Addr) bool {
 	flatAddr := addr.Flatid()
-	var already_acquired = false
-	for _, acq := range twophase.acquired {
-		if flatAddr == acq {
-			already_acquired = true
-		}
-	}
-	return already_acquired
+	return twophase.acquired[flatAddr]
 }
 
 func (twophase *TwoPhase) Acquire(addr addr.Addr) {
@@ -65,15 +58,9 @@ func (twophase *TwoPhase) Acquire(addr addr.Addr) {
 	}
 }
 
-func (twophase *TwoPhase) Release() {
-	last_index := len(twophase.acquired) - 1
-	twophase.locks.Release(twophase.acquired[last_index])
-	twophase.acquired = twophase.acquired[:last_index]
-}
-
 func (twophase *TwoPhase) ReleaseAll() {
-	for len(twophase.acquired) != 0 {
-		twophase.Release()
+	for flatAddr := range twophase.acquired {
+		twophase.locks.Release(flatAddr)
 	}
 }
 
