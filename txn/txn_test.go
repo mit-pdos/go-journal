ef67795a627aff577d2f5ef75f5be76facd918fb
Tej Chajed <tchajed@mit.edu>
2022-04-28 08:44:06 -0400

    Add a basic sanity test for txn

---
+++ txn/txn_test.go
@@ -0,0 +1,42 @@
+package txn_test
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/mit-pdos/go-journal/addr"
+	"github.com/mit-pdos/go-journal/txn"
+	"github.com/stretchr/testify/assert"
+	"github.com/tchajed/goose/machine/disk"
+)
+
+func data(sz int) []byte {
+	d := make([]byte, sz)
+	rand.Read(d)
+	return d
+}
+
+func blockAddr(a uint64) addr.Addr {
+	return addr.Addr{
+		Blkno: a,
+		Off:   0,
+	}
+}
+
+const blockSz uint64 = 8 * 4096
+
+func TestReadWrite(t *testing.T) {
+	d := disk.NewMemDisk(10000)
+	tsys := txn.Init(d)
+
+	x := data(4096)
+
+	tx := txn.Begin(tsys)
+	tx.OverWrite(blockAddr(513), blockSz, x)
+	tx.Commit()
+
+	tx = txn.Begin(tsys)
+	buf := tx.ReadBuf(blockAddr(513), blockSz)
+	assert.Equal(t, x, buf, "read incorrect data")
+	tx.ReleaseAll()
+}
